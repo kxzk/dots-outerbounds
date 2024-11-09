646847b740d337ffc6bf68975a58bc18d836001a
kade <kadekillary@pm.me>
2024-11-09 06:21:05 -0800

    Update init.lua

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -3,12 +3,12 @@ return {
 		"lewis6991/gitsigns.nvim",
 		opts = {
 			signs = {
-				add = { text = "|" },
-				change = { text = "+" },
-				delete = { text = "×" },
-				topdelete = { text = "×" },
-				changedelete = { text = "×" },
-				untracked = { text = "¦" },
+				add = { text = "│" },
+				change = { text = "│" },
+				delete = { text = "│" },
+				topdelete = { text = "│" },
+				changedelete = { text = "│" },
+				untracked = { text = "│" },
 			},
 		},
 	},
