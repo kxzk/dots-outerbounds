ae5ec9d382bdab976c083542fb32afc0c971f702
kade <kadekillary@pm.me>
2024-11-12 17:52:08 -0800

    Update statusline.lua

--- nvim/lua/configs/statusline.lua
+++ nvim/lua/configs/statusline.lua
@@ -1,16 +1,3 @@
-local function branch_name()
-	local branch = io.popen("git rev-parse --abbrev-ref HEAD 2> /dev/null")
-	if branch then
-		local name = branch:read("*l")
-		branch:close()
-		if name then
-			return name
-		else
-			return ""
-		end
-	end
-end
-
 local function file_path()
 	local path_to_file = vim.fn.pathshorten(vim.fn.getcwd()):lower()
 	return path_to_file .. "/"
@@ -21,24 +8,27 @@ local function hl(group, fg, bg)
 end
 
 -- colorscheme: neovim 0.10 default
-hl("StatusColor1", "#4f5258", "#1c1d23")
 hl("StatusColor", "#2c2e33", "#1c1d23")
+hl("StatusColor1", "#4f5258", "#1c1d23")
+hl("StatusColor2", "#FFCAFF", "#1c1d23")
 
 function status_line()
+	if vim.bo.filetype == "sql" then
+		return "%f"
+	end
+
 	return table.concat({
 		"%#StatusColor1#",
 		" ",
 		"%<",
-		branch_name(),
 		"%#StatusColor#",
 		"%=",
 		file_path(),
 		"%#StatusColor1#",
 		"%f",
-		"%#StatusColor#",
+		"%#StatusColor2#",
 		"%=",
 		"%m",
-		"%y",
 		" ",
 	})
 end
