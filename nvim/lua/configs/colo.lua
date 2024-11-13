8a35206fe8ff482730904daf83513f54e6fa1b29
kade <kadekillary@pm.me>
2024-11-12 20:40:46 -0800

    Update colo.lua

--- nvim/lua/configs/colo.lua
+++ nvim/lua/configs/colo.lua
@@ -1,5 +1,5 @@
 -- my variations to the new neovim default colorscheme
-vim.api.nvim_set_hl(0, "Comment", { fg = "#2c2e33" })
+vim.api.nvim_set_hl(0, "Comment", { fg = "#FCE094" })
 vim.api.nvim_set_hl(0, "EndOfBuffer", { fg = "#14161b" })
 vim.api.nvim_set_hl(0, "CursorLine", { bg = "#1c1d23" })
 vim.api.nvim_set_hl(0, "CursorLineNr", { fg = "#4f5258" })
